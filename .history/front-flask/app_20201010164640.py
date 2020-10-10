3713258f05ca7cd58eaa38df3cfdbe75f9b2142e
mswedrowski <swedrowskimichalms@gmail.com>
2020-10-10 18:08:32 +0200

    Final veresion

---
+++ .history/front-flask/app_20201010164640.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path="/templates")
+app._static_folder ='front-flask\static'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run()
